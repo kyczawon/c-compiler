ffe92b5b1a7eb009ff5636dc8841960513e6bf0a
Your Name <you@example.com>
2018-02-27 17:11:57 +0000

    Add parameter lists to functions

--- include/ast/ast_declarations.hpp
+++ include/ast/ast_declarations.hpp
@@ -5,14 +5,14 @@
 
 #include <cmath>
 
-class Function
+class NullaryFunction
     : public Expression
 {
 protected:
     ExpressionPtr sequence;
     std::string type, identifier;
 public:
-    Function(std::string &_type, std::string &_identifier, ExpressionPtr _sequence)
+    NullaryFunction(std::string &_type, std::string &_identifier, ExpressionPtr _sequence)
         : type(_type)
         , identifier(_identifier)
         , sequence(_sequence)
@@ -21,8 +21,39 @@ public:
     }
     virtual void print(int level, std::ostream &dst) const override
     {
-        std::cout << getStack()["f"] << std::endl;
         dst<<std::string(level,'\t')<<"def "<<identifier<<"():"<<std::endl;
+        sequence->print(level+1, dst);
+    }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    {
+        // NOTE : This should be implemented by the inheriting function nodes, e.g. LogFunction
+        throw std::runtime_error("FunctionOperator::evaluate is not implemented.");
+    }
+};
+
+class Function
+    : public Expression
+{
+protected:
+    ExpressionPtr sequence, parameter_list;
+    std::string type, identifier;
+public:
+    Function(std::string &_type, std::string &_identifier, ExpressionPtr _parameter_list, ExpressionPtr _sequence)
+        : type(_type)
+        , identifier(_identifier)
+        , parameter_list(_parameter_list)
+        , sequence(_sequence)
+    {
+        getStack()[identifier] = (ExpressionPtr) this;
+    }
+    virtual void print(int level, std::ostream &dst) const override
+    {
+        dst<<std::string(level,'\t')<<"def "<<identifier<<"(";
+        parameter_list->print(0, dst);
+        dst<<"):"<<std::endl;
         sequence->print(level+1, dst);
     }
 
@@ -76,6 +107,51 @@ public:
     virtual void print(int level, std::ostream &dst) const override
     {
         dst<<id<<"=0";
+    }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    {
+        return bindings.at(id);
+    }
+};
+
+class ParameterList
+    : public Expression
+{
+protected:
+    ExpressionPtr list, paramter;
+public:
+    ParameterList (ExpressionPtr _list, ExpressionPtr _paramter)
+            : list(_list),
+            paramter(_paramter)
+        {}
+    virtual void print(int level, std::ostream &dst) const override
+    {
+        list->print(0,dst);
+        dst << ",";
+        paramter->print(0,dst);
+    }
+};
+
+class Parameter
+    : public Expression
+{
+private:
+    std::string type, id;
+public:
+    Parameter(const std::string &_type, const std::string &_id)
+        : type(_type),
+        id(_id)
+    {}
+
+    const std::string getId() const
+    { return id; }
+
+    virtual void print(int level, std::ostream &dst) const override
+    {
+        dst<<id;
     }
 
     virtual double evaluate(
